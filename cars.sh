1e073b242d29f673ea896f38c586abad444ae1e8
Curren Taber <currentaber@gmail.com>
2018-12-02 20:30:48 -0800

    Adds cars.sh

--- cars.sh
+++ cars.sh
@@ -2,37 +2,44 @@
 # cars.sh
 # Curren Taber
 
-echo "Welcome"
-echo "1. Enter a Car"
-echo "2. List the Cars"
-echo "Q. Quit"
-
+cars=My_old_cars
+echo
+echo "Welcome. What would you like to do?"
+echo "[1] Enter a Car"
+echo "[2] List the Cars"
+echo "[Q] Quit"
+echo
 read answer
 
-while [ "$answer" != "Q" ]
+while [ "$answer" != "Q" ] && [ "$answer" != "q" ]
 do
-	case $answer in
-		"1")
-			echo "Year?"
-			read year
-			echo "Make?"
-			read make
-			echo "Model?"
-			read model
-			
-			echo year:make:model >> My_old_cars;;
-		"2")
-			echo cool;;
-
-	echo "1. Enter a Car"
-	echo "2. List the Cars"
-	echo "Q. Quit"
-	read answer	
+        case $answer in
+                "1")
+                        echo
+                        echo "Year?"
+                        read year
+                        echo
+                        echo "Make?"
+                        read make
+                        echo
+                        echo "Model?"
+                        read model
+                        echo $year:$make:$model >> My_old_cars
+                        ;;
+                "2")
+                        echo
+                        echo "My Old Cars:"
+                        sort $cars | tr ":" " "
+                        ;;
+        esac
+        echo
+        echo "What would you like to do?"
+        echo "[1] Enter a Car"
+        echo "[2] List a Car"
+        echo "[Q] Quit"
+        echo
+        read answer
 done
 
-#case expression in
-#	pattern1 )
-#		statements ;;
-#	pattern2 )
-#		statements ;;
-#esac
+echo
+echo "Goodbye"
